4de0b53da3eb5433492261ee1c67156a0d7f4df7
abreternitz <aliceburian@gmail.com>
2018-06-19 14:11:30 +0000

    automatically backed up by learn

---
+++ bored.rb
@@ -0,0 +1,15 @@
+puts "Hey what's your name"
+name=gets.chomp.capitalize
+puts "Hi #{name}, I'm a computer. How are you?"
+emotion=gets.chomp.capitalize
+puts "You look like you need a vacation. Where do you work?"
+work=gets.chomp.capitalize
+puts "thats pretty impressive! How old are you?"
+age=gets
+puts "Ah a great year! What's your birthmonth?"
+month=gets.chomp.capitalize
+puts "Wow, thats coming up soon(ish)! Do you have any plans?"
+answer=gets.chomp.capitalize
+puts "I'm sure you'll have a great birthday!"
+
+puts "Well, #{name}, this was a great chat. Hope #{work} is fun!"
